b1997b8a912588be6d250a4aabdcdc8352382fe4
Mark van Dam <114491487+Markusvdam@users.noreply.github.com>
2022-11-03 17:18:14 +0100

    small update
    small update

--- src/components/organisms/TopHeader/TopHeader.js
+++ src/components/organisms/TopHeader/TopHeader.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import Hamburgermenu from './../../atoms/Hamburgermenu'
 import Logo from './../../atoms/Logo'
+import Cart from './../../atoms/Cart'
 
-/// TODO: Define props
 const TopHeader = ({ testID }) => {
 
     const utils = ["TopHeader"].join(" ")
@@ -11,8 +11,8 @@ const TopHeader = ({ testID }) => {
 
     return(
         <div data-testid={ testID } className={ utils }>
-            <><Logo /></>
-            <><Hamburgermenu /></>
+            <div className="left"><Logo /></div>
+            <div className="right"><Cart /><Hamburgermenu /></div>
         </div>
     )
 }
